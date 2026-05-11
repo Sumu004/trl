50391ad037e3a5ed09d8a14614bb280f012c72de
aminediro <amine.dirhoussi@huggingface.co>
2026-05-11 20:32:55 +0000

    test: drop dead stub methods (pause/resume/send_weights now on WeightTransferClient)

--- tests/experimental/test_async_grpo_trainer.py
+++ tests/experimental/test_async_grpo_trainer.py
@@ -81,15 +81,6 @@ class _StubRolloutWorker:
     def stop(self):
         pass
 
-    def pause(self):
-        pass
-
-    def resume(self):
-        pass
-
-    def send_weights(self, iterator):
-        pass
-
 
 class TestAsyncGRPOTrainer(TrlTestCase):
     def test_init_minimal(self):
